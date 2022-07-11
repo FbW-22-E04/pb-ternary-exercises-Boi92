44aa8e2d42294ab6482b2aff29ad736810436bd9
Boi Schrader <boi1992@t-online.de>
2022-07-11 14:48:23 +0200

    exercise done

---
+++ solution.js
@@ -0,0 +1,25 @@
+ const isDog = true ;
+
+ console.log(isDog == true ? "pat,pat" : "find me a dog to pat!") 
+
+ const speedLimit = 50;
+ const yourSpeed = 49;
+
+ console.log(yourSpeed > speedLimit ? "you're going to fast!" : "You're driving below the speed limit, Oma");
+
+ const age = 29
+ console.log(age < 16 ? "serve butter beer" : "serve beer");
+
+ const isStudent = false;
+ console.log(isStudent == true ? "Ticket costs €5.00" : "Ticket costs €12.00")
+
+ const okMarie = "nocake"
+ console.log(okMarie == "nocake" ? "Let them eat cake" : "Oh, bother")
+
+ const num1 = 30
+ const num2 = 939
+ const num3 = 40.9
+
+ console.log(num1 % 2 === 0 ? `${num1} is even` : `${num1} is odd`)
+ console.log(num2 % 2 === 0 ? `${num2} is even` : `${num2} is odd`)
+ console.log(num3 % 2 === 0 ? `${num3} is even` : `${num3} is odd`)
